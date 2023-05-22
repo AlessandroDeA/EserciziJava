d0395a7007d496609be4130376bb81b12ffabe9a
Alessandro <deangelisalessandro2000@gmail.com>
2023-05-22 22:22:20 +0200

    Esercizi Java

---
+++ Stringhe/Punteggiatura.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Punteggiatura {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str;
+        int cont = 0;
+
+        while(true)
+        {
+            System.out.println("Inserisci una parola contenente punteggiatura:");
+            str = input.nextLine();
+            if(str.isEmpty())
+                break;
+                for (int i = 0; i < str.length(); i++) {
+                    char ch = str.charAt(i);
+                    if (ch == '.' || ch == ',' || ch == ';' || ch == ':' || ch == '!' || ch == '?') {
+                        cont++;
+                    }
+                }
+            }
+            input.close();
+            System.out.println("Numero di punteggiature trovate: " + cont);
+        }
+}
